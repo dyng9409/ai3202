4900c3b9af9a52d8ab73d02b6d091c01189e0867
Dylan Nguyen <dyng9409@colorado.edu>
2015-09-11 15:59:16 -0600

    added world definition files

--- Assignment2/node.py
+++ Assignment2/node.py
@@ -4,9 +4,9 @@
 
 class Node(object):
     
-    def ___init___(self):
-        self.location = (0,0)
+    def ___init___(locx,locy,heur):
+        self.location = (locx,locy)
         self.distanceToStart = 0
-        self.heuristic = 0
+        self.heuristic = heur
         self.f = 0
         self.parent = None
