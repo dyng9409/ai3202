2795b4fbd1fb1725ae65a2260056a5722041935f
Dylan Nguyen <dyng9409@colorado.edu>
2015-09-26 21:38:07 -0600

    updated map parsing for MDP

--- Assignment5/mapParse.py
+++ Assignment5/mapParse.py
@@ -18,14 +18,27 @@ def parse(f):
     #remove any blank lines
     while sq.count([]) > 0:
         sq.remove([])
-
+    sq.reverse()
     #now we're left with a list of lists with the main
     #property of each square
-    
+    #TODO maybe switch the y so its more intuivitve mapping 
     #turn each element into a Node object with more descriptive data
     for y in range(0,len(sq)):
         for x in range(0,len(sq[0])):
-            sq[y][x] = world.Node(sq[y][x],x,y)
+            if sq[y][x] == '0':
+                sq[y][x] = world.Node('plain',x,y)
+            elif sq[y][x] == '1':
+                sq[y][x] = world.Node('mountain',x,y)
+            elif sq[y][x] == '2':
+                sq[y][x] = world.Node('wall',x,y)
+            elif sq[y][x] == '3':
+                sq[y][x] = world.Node('snake',x,y)
+            elif sq[y][x] == '4':
+                sq[y][x] = world.Node('barn',x,y)
+            else:
+                sq[y][x] = world.Node('goal',x,y)
+
+
 
 
     #finally, creating a mroe concrete map object for us to work with
--- Assignment5/world.py
+++ Assignment5/world.py
@@ -1,6 +1,7 @@
 #implementation of node and
 #graph data strcuture for
 #a* search
+#revised for MDP VI
 
 class Node(object):
     
@@ -13,32 +14,6 @@ class Node(object):
         self.f = 0
         self.parent = None
 
-    def xval(self):
-        return self.x
-
-    def yval(self):
-        return self.y
-
-    def fval(self):
-        return self.f
-
-    def distVal(self):
-        return self.distanceToStart
-
-    def hval(self):
-        return self.heuristic
-
-    def setF(self, val):
-        self.f = val
-
-    def setParent(self,p):
-        self.parent = p
-
-    def setDistance(self,val):
-        self.distanceToStart = val
-
-    def setHeuristic(self,val):
-        self.heuristic = val
 
 class Map(object):
 
@@ -55,34 +30,3 @@ class Map(object):
     def get(self,x,y):
         #returns the node at coordinates x,y
         return self.map[y][x]
-
-    def applyHeuristic1(self):
-        for y in range(0,self.ybound):
-            for x in range(0,self.xbound):
-                n = self.get(x,y)
-                heur = ((self.xbound-x-1)+(self.ybound-y-1))*10
-                n.setHeuristic(heur)
-
-    def applyHeuristic2(self):
-        limval = 0
-        if self.ybound > self.xbound:
-            limval = self.ybound-1
-        else:
-            limval = self.xbound-1
-        for y in range(0,self.ybound):
-            for x in range(0,self.xbound):
-                n = self.get(x,y)
-                #TODO: new heuristic
-                selfx = n.xval()
-                selfy = n.yval()
-                sumcoord = x+y
-                heur = 0
-                if sumcoord == limval:
-                    heur = 14*selfy
-                elif sumcoord < limval:
-                    heur = 14*selfy+10*(self.xbound-(selfx+selfy)-1)
-                else:
-                    heur = 14*(self.xbound-selfx-1)+10*(selfy-(self.xbound-selfx-1))
-
-                n.setHeuristic(heur)
-
