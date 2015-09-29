1de6568a3f877ea3ba3242567a218f528327f8bf
Dylan Nguyen <dyng9409@colorado.edu>
2015-09-28 19:02:32 -0600

    answered questions, now to wait and compare

--- Assignment5/mdpvi.py
+++ Assignment5/mdpvi.py
@@ -15,4 +15,29 @@ worldsource = args.worldfile[0]
 wmap = mapParse.parse(worldsource)
 
 vi.valueIteration(wmap, eps)
-wmap.printOptimal()
+start = wmap.get(0,0)
+goal = wmap.get(wmap.xbound, wmap.ybound)
+utility = 0
+curNode = start
+
+while curNode.typ is not 'goal':
+    curU = curNode.utility
+    utility = utility + curU
+    curx = curNode.x
+    cury = curNode.y
+    if curx==0 and cury==0:
+        print (curx, cury), ' START'
+    else:
+        print(curx, cury), 'Current Utility: ',round(utility,3)
+    nextmove = curNode.optimal
+    if nextmove is 'left':
+        curNode = wmap.get(curx-1, cury)
+    elif nextmove is 'right':
+        curNode = wmap.get(curx+1, cury)
+    elif nextmove is 'up':
+        curNode = wmap.get(curx,cury+1)
+    else:
+        curNode = wmap.get(curx,cury-1)
+print (curNode.x, curNode.y),' FINISH'
+utility = curNode.utility + utility
+print 'Final Utility: ', round(utility,3)
